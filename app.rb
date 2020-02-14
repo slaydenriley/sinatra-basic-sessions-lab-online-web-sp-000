3795f0814ff066304c2ef574ef791a251fc4f0d3
Riley Slayden <slaydenriley@gmail.com>
2020-02-14 17:55:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "G+cd4)Lm7N:?w2w"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+
 end
